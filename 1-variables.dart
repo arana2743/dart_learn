e5c6cc8519cd4782f2de550403fb8b9504ca24b3
arana2743 <abhisekrana777@gmail.com>
2022-07-29 13:23:40 +0530

    variables and data-types

---
+++ 1-variables.dart
@@ -0,0 +1,6 @@
+void main() {
+  String name = 'John';
+  int age = 10;
+  print("The name is ${name} Cena!"); // string interpolation
+  print("And he is ${age} years old.");
+}
---
+++ 2-dataTypes.dart
@@ -0,0 +1,15 @@
+// Dart data-types:
+// 1. Strings
+// 2. Numbers - int and double
+// 3. boolean
+void main() {
+  String name = "John Dee";
+  int age = 20;
+  double price = 10.234;
+  bool isWorkDone = false;
+
+  print(name);
+  print(age);
+  print(price);
+  print(isWorkDone);
+}
