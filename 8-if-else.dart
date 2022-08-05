46926eb14515e90f174ae9ea1f264b0aea354b3a
arana2743 <abhisekrana777@gmail.com>
2022-08-05 13:53:54 +0530

    conditionals and logical operators

---
+++ 8-if-else.dart
@@ -0,0 +1,11 @@
+void main() {
+  print("I'm at resturant!");
+  String foodChoice = "pasta";
+  if (foodChoice == "meat") {
+    print("I want steak!");
+  } else if (foodChoice == "pasta") {
+    print("I want spaghetti and meatballs!");
+  } else {
+    print("I want salad");
+  }
+}
---
+++ 9-logical-operators.dart
@@ -0,0 +1,5 @@
+void main() {
+  print((10 < 20) && (false));
+  print(true || false);
+  print(!true);
+}
